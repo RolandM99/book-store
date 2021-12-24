21cf5b158ccd52b567916ce22c00806a0941e7da
rolandm99 <manfulmweze99@gmail.com>
2021-12-24 21:05:44 +0100

    Add middleware and side effects to allow redux to use async functions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,25 +1,50 @@
+import { createBook, removeBook, getBooks } from '../../api/addApi';
+
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+const RETRIEVE_BOOKS = 'bookStore/books/RETRIEVE_bOOKS';
 
 const initialState = [];
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
+export const addBook = (book) => async (dispatch) => {
+  await createBook(book);
+  dispatch({
+    type: ADD_BOOK,
+    payload: book,
+  });
+};
 
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
+export const deleteBook = (book) => async (dispatch) => {
+  await removeBook(book);
+  dispatch({
+    type: REMOVE_BOOK,
+    payload: book,
+  });
+};
+
+export const retrieveAllBooks = () => async (dispatch) => {
+  const data = await getBooks();
+  const mybooks = Object.entries(data).map(([key, value]) => {
+    const [booksDonnee] = value;
+    return {
+      item_id: key,
+      ...booksDonnee,
+    };
+  });
+  dispatch({
+    type: RETRIEVE_BOOKS,
+    payload: mybooks,
+  });
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
-
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload.id);
+    case RETRIEVE_BOOKS:
+      return [...state, ...action.payload];
     default:
       return state;
   }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,6 +4,7 @@ import {
   applyMiddleware,
 } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 
 const reducer = combineReducers({
@@ -11,9 +12,6 @@ const reducer = combineReducers({
   // additional reducers could be added here
 });
 
-const store = createStore(
-  reducer,
-  applyMiddleware(logger),
-);
+const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 export default store;
