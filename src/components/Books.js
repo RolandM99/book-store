0b4a5feeee3882fd5a3efce99b0d66014c177cad
rolandm99 <manfulmweze99@gmail.com>
2021-12-25 23:34:45 +0100

    Add style from Zeplin Bookstore design

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Proptypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/books';
 
@@ -15,31 +17,53 @@ const Books = (props) => {
     dispatch(removeBook(book));
   };
 
+  const circularBar = Math.round(Math.random() * 20);
+
   return (
     <>
-      <div id="book-page-container">
-        <ul id="books-container">
-          <li>
-            <div className="container-1">
-              <div>
-                <p className="book-category">{category}</p>
+      <div className="my-page">
+        <div id="book-page-container">
+          <ul id="books-container">
+            <li>
+              <div className="container-1">
+                <div>
+                  <p className="book-category">{category}</p>
+                </div>
+                <div>
+                  <p className="book-title">{title}</p>
+                </div>
+                <div className="commentaire">
+                  <div className="comment">Comments</div>
+                  <button type="submit" className="remove-btn" onClick={() => deleteBook(id)}>
+                    Remove
+                  </button>
+                  <div className="edit-book">Edit</div>
+                </div>
               </div>
-              <div>
-                <p className="book-title">{title}</p>
+              <div className="container-2">
+                <div style={{ width: 75, height: 75 }}>
+                  <CircularProgressbar value={Math.round((circularBar / 20) * 100)} />
+                </div>
+                <div className="pourcent">
+                  <p className="percentageVal">
+                    {Math.round((circularBar / 20) * 100)}
+                    %
+                  </p>
+                  <p id="completed">completed</p>
+                </div>
               </div>
-              <div className="commentaire">
-                <div className="commentaire">Comments</div>
-                <button type="submit" className="remove-btn" onClick={() => deleteBook(id)}>
-                  REMOVE
-                </button>
-                <div className="edit-book">Edit</div>
+              <div className="container-3">
+                <span>CURRENT CHAPTER</span>
+                <p id="my-chap">
+                  Chapter
+                  {' '}
+                  {circularBar}
+                </p>
+                <button id="upd-btn" type="button">UPDATE PROGRESS</button>
               </div>
-            </div>
-            <div className="container-2">
-              <div className="Oval-2">.</div>
-            </div>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
